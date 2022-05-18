abd8017ff43290dddc25a30cdccad6158f60f6cb
tomgrossman <tomg1988@gmail.com>
2022-05-18 11:56:07 +0300

    version 3
    used `turbo-http`

--- index.js
+++ index.js
@@ -1,32 +1,38 @@
 const fs = require('fs');
-const fastify = require('fastify')()
+const turbo = require('turbo-http')
 const port = +process.argv[2] || 3000
 
 const cardsData = fs.readFileSync('./cards.json');
 const cards = JSON.parse(cardsData);
 const cardsLength = cards.length;
 
-fastify.get('/card_add', async (req, reply) => {
-    const key = `user_id:${req.query.id}`;
-    const userCardCount = await client.incr(key);
-    if (cardsLength < userCardCount) {
-        return { id: "ALL CARDS" }
+async function handleRequests (req, res) {
+    if ('/ready' === req.url) {
+        res.setHeader('Content-Length', 19)
+        return res.write('{ "ready": "true" }')
     }
 
-    return cards[userCardCount-1];
-})
+    const userCardCount = await client.incr(req.url);
+    if (cardsLength < userCardCount) {
+        res.setHeader('Content-Length', 21)
+        return res.write('{ "id": "ALL CARDS" }');
+    }
 
-fastify.get('/ready', async (req, reply) => {
-    return { ready: true }
-})
+    const card = cards[userCardCount-1];
+
+    const response = `{ "id": "${card.id}", "name": "${card.name}"}`;
+    res.setHeader('Content-Length', response.length)
+    return res.write(response);
+}
 
 const client = require('redis').createClient()
 client.on('error', (err) => console.log('Redis Client Error', err));
 
 client.on('ready', async () => {
     try {
-        await fastify.listen(port);
-        console.log(`Example app listening at http://0.0.0.0:${port}`)
+        const server = turbo.createServer(handleRequests)
+        server.listen(port)
+        console.log(`Server is running on http://0.0.0.0:${port}`);
     } catch (error) {
         console.error(error)
     }
