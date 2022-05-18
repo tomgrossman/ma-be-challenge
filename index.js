1bae850b4ab86f2917e6375170e2bf0042418cdd
tomgrossman <tomg1988@gmail.com>
2022-05-18 10:35:28 +0300

    version 2
    changed to INCR instead of saving the cards in redis

--- index.js
+++ index.js
@@ -4,35 +4,16 @@ const port = +process.argv[2] || 3000
 
 const cardsData = fs.readFileSync('./cards.json');
 const cards = JSON.parse(cardsData);
-const cardsStrings = cards.map(card => JSON.stringify(card));
-const allCardsKey = 'all_cards';
-
-let insertedCards = false;
-
-async function upsertUser (userKey) {
-    // This should have been during the initialization of the app, but the tests file flush redis after the app is ready :/
-    // could have skipped this entire block
-    if (!insertedCards) {
-        await client.sAdd(allCardsKey, cardsStrings)
-        insertedCards = true;
-    }
-
-    const result = await client.sAdd('all_users', userKey);
-    if (1 === result) {
-        // if the user was first created, add all available cards to this user
-        await client.copy(allCardsKey, userKey);
-    }
-}
+const cardsLength = cards.length;
 
 fastify.get('/card_add', async (req, reply) => {
-    const  key = `user_id:${req.query.id}`;
-    await upsertUser(key);
-    const missingCard = await client.sPop(key);
-    if (!missingCard) {
+    const key = `user_id:${req.query.id}`;
+    const userCardCount = await client.incr(key);
+    if (cardsLength < userCardCount) {
         return { id: "ALL CARDS" }
     }
 
-    return missingCard;
+    return cards[userCardCount-1];
 })
 
 fastify.get('/ready', async (req, reply) => {
